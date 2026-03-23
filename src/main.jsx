6441ba76349ed64eb4a76bd77bf1e647688d792e
Jodi <jodi.gunawan@telus.com>
2026-03-22 18:05:22 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PaintMyBed from '../paint-my-bed-fixed.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PaintMyBed />
+  </React.StrictMode>,
+)
